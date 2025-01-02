fe329b41028eccc490cd021a0c9539133893278f
rowanhere <lamsalgokul917@gmail.com>
2025-01-02 21:00:07 +0545

    add and remove update v2

--- userRoutes/addToWishList.js
+++ userRoutes/addToWishList.js
@@ -3,27 +3,38 @@ import User from "../Models/user.model.js";
 const addToWishList = async (req, res) => {
   const { ProductId } = req.body;
   const userId = req.id;
-  try{
-  const doc = await User.findOne({ _id: userId });
-  const likedId = doc.liked;
-  if (likedId.has(ProductId) && likedId.delete(ProductId)) {
+  try {
+    const doc = await User.findOne({ _id: userId });
+    const likedId = doc.liked;
+    if (likedId.has(ProductId) && likedId.delete(ProductId)) {
+      doc.liked = likedId;
+      await doc.save();
+      res.send("Removed from wishlist");
+      console.log(
+        "userId:",
+        userId,
+        "ProductID:",
+        ProductId,
+        "(Removed from wishlist)"
+      );
+
+      return;
+    }
+    likedId.set(ProductId, true);
     doc.liked = likedId;
     await doc.save();
-    res.send("Removed from wishlist");
-  console.log("userId:",userId, "ProductID:", ProductId,"(Removed from wishlist)");
-
-    return;
+    console.log(
+      "userId:",
+      userId,
+      "ProductID:",
+      ProductId,
+      "(Added to wishlist)"
+    );
+    res.send("Added to wishlist");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal server error!");
   }
-  likedId.set(ProductId,true)
-  doc.liked = likedId
-  await doc.save()
-  console.log("userId:",userId, "ProductID:", ProductId,"(Added to wishlist)");
-  res.send("Added to wishlist");
-}catch(err){
-  console.log(err);
-  res.status(500).send("Internal server error!")
-  
-}
 };
 
 export default addToWishList;
