c2a656d37c3be559809e26046abd93b95911b714
rowanhere <lamsalgokul917@gmail.com>
2025-01-02 20:59:38 +0545

    add and remove update

--- userRoutes/addToWishList.js
+++ userRoutes/addToWishList.js
@@ -3,11 +3,27 @@ import User from "../Models/user.model.js";
 const addToWishList = async (req, res) => {
   const { ProductId } = req.body;
   const userId = req.id;
-  console.log(userId,ProductId);
-  await User.findOneAndUpdate({_id:userId},{
-    "$set":{[`liked.${ProductId}`]:true}
-  })
-  res.send("Adden to wishlist");
+  try{
+  const doc = await User.findOne({ _id: userId });
+  const likedId = doc.liked;
+  if (likedId.has(ProductId) && likedId.delete(ProductId)) {
+    doc.liked = likedId;
+    await doc.save();
+    res.send("Removed from wishlist");
+  console.log("userId:",userId, "ProductID:", ProductId,"(Removed from wishlist)");
+
+    return;
+  }
+  likedId.set(ProductId,true)
+  doc.liked = likedId
+  await doc.save()
+  console.log("userId:",userId, "ProductID:", ProductId,"(Added to wishlist)");
+  res.send("Added to wishlist");
+}catch(err){
+  console.log(err);
+  res.status(500).send("Internal server error!")
+  
+}
 };
 
 export default addToWishList;
