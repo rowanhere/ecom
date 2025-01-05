53fba2be4efc9c432cca55072d95862972a596b8
rowanhere <lamsalgokul917@gmail.com>
2025-01-05 18:35:51 +0545

    Edited cart functions

--- userRoutes/HandleCart.js
+++ userRoutes/HandleCart.js
@@ -1,16 +1,31 @@
+import e from "express";
+import Product from "../Models/products.model.js";
 import User from "../Models/user.model.js";
 
 const getCart = async (req, res) => {
   try {
-    const user = await User.findOne({ _id: req.id });
+    const user = await User.findById(req.id);
     const cart = user.cart;
-    if (req.method === "GET") {
-      console.log(user.cart.size);
-      if (cart.size === 0) {
-        return res.send("Cart is empty");
-      }
-      return res.status(200).send(user.cart);
-    }
+    if (cart.length === 0) return res.send([]);
+    const onlyKeys = [...cart.keys()];
+    const product = await Product.find({
+      _id:{$in:onlyKeys}
+    }).select([
+      "category",
+      "title",
+      "_id",
+      "id",
+      "price",
+      "discount",
+      "thumbnail",
+    ]);
+    //add amount on product
+    const amtAdded = product.map((e)=> {
+      const getAmt = cart.get(e._id)
+      return {...e._doc,amount:getAmt.amount}
+    })
+    
+     res.status(200).send(amtAdded);
   } catch (err) {
     console.log(err);
 
@@ -22,11 +37,14 @@ const addToCart = async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.id });
     const cart = user.cart;
-    const { _id, id, title, thumbnail, amount } = req.body;
-    if (!(_id, id && title && thumbnail && amount)) {
+    const { productId } = req.body;
+    if (!productId) {
       return res.status(400).send("Invalid product to add on cart!");
     }
-    cart.set(_id, { id, title, thumbnail, amount });
+    let amt = 0;
+    if (cart.has(productId)) amt = parseInt(cart.get(productId).amount);
+
+    cart.set(productId, { amount: ++amt });
     user.cart = cart;
     await user.save();
     res.send(cart);
@@ -48,10 +66,9 @@ const updateCart = async (req, res) => {
       return res.status(400).send("Invalid request");
     }
 
-    getRequireProduct.amount += parseAmount;
+    getRequireProduct.amount = parseAmount;
     if (getRequireProduct.amount <= 0) {
       cart.delete(productId);
-      console.log(cart);
     } else {
       cart.set(productId, getRequireProduct);
     }
@@ -83,4 +100,4 @@ const deleteCart = async (req, res) => {
     res.status(500).send("Internal server error");
   }
 };
-export { getCart, addToCart, updateCart,deleteCart };
+export { getCart, addToCart, updateCart, deleteCart };
