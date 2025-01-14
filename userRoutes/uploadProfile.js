0ebaa0752b9eec2bcdf01425912bbfd71818d988
rowanhere <lamsalgokul917@gmail.com>
2025-01-14 20:40:24 +0545

    using api of imgbb

--- userRoutes/uploadProfile.js
+++ userRoutes/uploadProfile.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import User from "../Models/user.model.js";
 import { Binary } from "mongodb";
 const uploadProfile = async (req, res) => {
@@ -9,17 +10,26 @@ const uploadProfile = async (req, res) => {
       return res.status(400).send("Only images allowed!");
     const fileSize = getFile.size / (1024 * 1024); //converting kb to mb
     if (fileSize > 5) return res.status(400).send("File too large!"); //checking size in mb (max= 5mb)
-    const user = await User.findByIdAndUpdate(
-      Id,
-      {
-        picture: {
-          image: getFile.buffer,
-        },
-      },
-      { new: true }
-    );
+    const formData = new FormData();
+    formData.append("key", "02f7298a13653f3f28dc464251fb6e6c");
+    formData.append("image", getFile.buffer.toString("base64"));
 
-    console.log(user);
+    const uploadImage = await axios.post(
+      "https://api.imgbb.com/1/upload",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    const recievedImageUrl = uploadImage.data.data.url;
+
+    await User.findByIdAndUpdate(Id, {
+      picture: {
+        image: recievedImageUrl,
+      },
+    });
 
     res.send("Image uploaded successfully");
   } catch (err) {
